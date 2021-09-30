e459f316096e8105fdb0e1003c16e0259d3525c5
Wojciech Borawski <w.borawski@ihar.edu.pl>
2021-09-30 15:02:03 +0200

    finished project

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ question_bank = []
 for data in question_data:
     question_bank.append(Question(text=data["text"], answer=data["answer"]))
 
-quiz = QuizBrain(question_bank)
+quiz = QuizBrain(question_list=question_bank)
 
 while quiz.still_has_question():
     quiz.next_question()
+
+quiz.final_score()
